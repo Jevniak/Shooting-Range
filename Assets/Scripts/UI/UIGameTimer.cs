81d3bab966ea4003884c60096bad879e06fa9238
Jevniak <jevniak12@gmail.com>
2021-09-22 22:17:54 +0300

    add UI, light in level, update scene

---
+++ Assets/Scripts/UI/UIGameTimer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using Target;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UIGameTimer : MonoBehaviour
+    {
+        public static bool GameStarted;
+
+        [SerializeField, Header("Время игры в секундах")]
+        private int timeToEnd;
+
+        [SerializeField, Header("Ссылка на текстовое поле для таймера")]
+        private TMP_Text timer;
+
+        [SerializeField, Header("Окно игры")] 
+        private GameObject windowGame;
+
+        [SerializeField, Header("Окно с результатом")] 
+        private GameObject windowResult;
+
+        [SerializeField] private Button buttonMainMenu;
+
+        private void Awake()
+        {
+            buttonMainMenu.onClick.AddListener(() =>
+            {
+                SceneManager.LoadScene("MainMenu");
+            });
+        }
+
+        private void Start()
+        {
+            GameStarted = true;
+            StartCoroutine(Timer());
+        }
+
+        private IEnumerator Timer()
+        {
+            while (timeToEnd > 0)
+            {
+                string minutes = Math.Truncate((decimal)(timeToEnd / 60)).ToString(CultureInfo.InvariantCulture);
+                string seconds = (timeToEnd % 60).ToString();
+                timer.text = $"{minutes}:{(seconds.Length == 1 ? 0 + seconds : seconds)}";
+                timeToEnd--;
+                yield return new WaitForSeconds(1);
+            }
+            timer.text = "0:00";
+            FinishGame();
+        }
+
+        private void FinishGame()
+        {
+            GameStarted = false;
+            windowGame.SetActive(false);
+            windowResult.SetActive(true);
+            ScoreSystem.Inst.SaveCurrentPlayer();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIMainMenuManager.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sound;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UIMainMenuManager : MonoBehaviour
+    {
+        [SerializeField] private AudioClip audioButtonClick;
+        
+        [SerializeField] private TMP_InputField username;
+        [SerializeField] private Toggle[] togglesDifficulty;
+        [SerializeField] private Button buttonPlay;
+        [SerializeField] private Button buttonExit;
+        [SerializeField] private TMP_Text loadingText;
+
+        [SerializeField] private List<GameObject> windowList;
+
+        private void Awake()
+        {
+            if (PlayerPrefs.HasKey("PlayerName"))
+                username.text = PlayerPrefs.GetString("PlayerName");
+            ChangeWindow(0);
+            
+            for (int i = 0; i < togglesDifficulty.Length - 1; i++)
+            {
+                togglesDifficulty[i].onValueChanged.AddListener((value) =>
+                {
+                    SetPlayerPrefsDifficulty();
+                    SoundManager.Inst.PlayAudio(audioButtonClick);
+                });
+            }
+            
+            
+
+            buttonPlay.onClick.AddListener(() =>
+            {
+                PlayerPrefs.SetString("PlayerName", username.text);
+                SoundManager.Inst.PlayAudio(audioButtonClick);
+                ChangeWindow(1);
+                StartCoroutine(LoadScene());
+            });
+            
+            buttonExit.onClick.AddListener(Exit);
+        }
+
+        private void Exit()
+        {
+            SoundManager.Inst.PlayAudio(audioButtonClick);
+            Application.Quit();
+        }
+        
+        private void ChangeWindow(int id)
+        {
+            /* id ==
+             * 0 - WindowMainMenu
+             * 1 - WindowProgressLoadLevel
+             */
+            foreach (GameObject window in windowList)
+            {
+                window.SetActive(false);
+            }
+
+            windowList[id].SetActive(true);
+        }
+
+        private IEnumerator LoadScene()
+        {
+            AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("Level");
+            asyncOperation.allowSceneActivation = false;
+            while (!asyncOperation.isDone)
+            {
+                loadingText.text = "Loading progress: " + (asyncOperation.progress * 100) + "%";
+
+                if (asyncOperation.progress >= 0.9f)
+                {
+                    loadingText.text = "Press left mouse button";
+                    if (Input.GetMouseButtonDown(0))
+                        asyncOperation.allowSceneActivation = true;
+                }
+
+                yield return null;
+            }
+        }
+
+        private void Start()
+        {
+            if (!PlayerPrefs.HasKey("Difficulty"))
+                SetPlayerPrefsDifficulty();
+            print(PlayerPrefs.GetInt("Difficulty"));
+            togglesDifficulty[PlayerPrefs.GetInt("Difficulty")].isOn = true;
+        }
+
+        private void SetPlayerPrefsDifficulty()
+        {
+            for (int i = 0; i < togglesDifficulty.Length; i++)
+            {
+                if (togglesDifficulty[i].isOn)
+                {
+                    PlayerPrefs.SetInt("Difficulty", i);
+                }
+            }
+            print("dif: "+PlayerPrefs.GetInt("Difficulty"));
+        }
+    }
+}
