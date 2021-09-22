90362de18e30a8b77b5e6db57670f09d383495ba
Jevniak <jevniak12@gmail.com>
2021-09-22 22:14:48 +0300

    Sound manager

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Sound
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class SoundManager : MonoBehaviour
+    {
+    
+    
+        private AudioSource _audioSource;
+    
+        public static SoundManager Inst;
+
+        private void Awake()
+        {
+            Inst = this;
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlayAudio(AudioClip audioClip)
+        {
+            _audioSource.clip = audioClip;
+            _audioSource.Play();
+        }
+
+        public void StopAudio()
+        {
+            _audioSource.Stop();
+        }
+    }
+}
